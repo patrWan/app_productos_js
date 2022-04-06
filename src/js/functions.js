5552388decb5ba1c28257cb2ee7280d958218a1e
Patricio González Camilo <patricio.gonzalez.camilo@gmail.com>
2022-04-05 23:50:17 -0400

    Implementación de los datos de la api.
    Se creó una función que rescata los datos de la api y los muestra en la aplicación.

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,3 +1,35 @@
+$(function () {
+    fetch('http://localhost:3000/api/product')
+        .then(response => response.json())
+        .then(data => {
+            data.map(product => {
+                let apply_discount = product.price - (product.price * product.discount / 100);
+                $("#products").append(
+                    `
+                    <div class="product">
+                        <div class="product-name">
+                            `+ product.name + `
+                        </div>
+                        <div class="product-image">
+                            <img src=" `+ product.url_image + `" alt="image_alt"/>
+                        </div>
+
+                        <div class="product-details">
+                            <div class="product-details-discount">
+                                <p id="price">$`+ product.price + ` &nbsp;</p> 
+                                <p id="new_price">$`+ apply_discount  + ` &nbsp;</p>
+                                <p id="discount">`+ product.discount + `% descuento</p>
+                            </div>
+                            <button type="button" class="btn btn-primary">Añadir al carrito</button>
+                        </div>
+                    </div>
+                    `
+                );
+            });
+
+        });
+});
+
 $(document).on("click", "#click", function () {
     $("#txt_nombres").text('hola');
 });
